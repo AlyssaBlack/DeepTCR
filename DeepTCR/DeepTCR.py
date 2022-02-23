d6818f0bb090163a7b8d3fe2d8d285962bca30f2
AlyssaBlack <alyssablackburn@uchicago.edu>
2022-02-22 21:42:58 -0600

    update
    trying to get past imports

--- DeepTCR/DeepTCR.py
+++ DeepTCR/DeepTCR.py
@@ -3,8 +3,8 @@ sys.path.append('../')
 from DeepTCR.DeepTCR.functions.Layers import *
 from DeepTCR.DeepTCR.functions.utils_u import *
 from DeepTCR.DeepTCR.functions.utils_s import *
-from DeepTCR.DeepTCR.functions.act_fun import *
-from DeepTCR.DeepTCR.functions.plot_func import *
+#from DeepTCR.DeepTCR.functions.act_fun import *
+#from DeepTCR.DeepTCR.functions.plot_func import *
 import seaborn as sns
 import colorsys
 from scipy.cluster.hierarchy import linkage,fcluster,dendrogram, leaves_list
